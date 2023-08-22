087f38571dc8595a88a2c182eace41981fc642d5
Robert Nemet <robert.nemet@gmail.com>
2023-08-22 09:39:24 +0200

    network and vms

--- gcp/network/main.tf
+++ gcp/network/main.tf
@@ -17,9 +17,45 @@ resource "google_compute_network" "back_office" {
 # subnet for back office
 resource "google_compute_subnetwork" "back_office" {
   name          = "back-office"
-  ip_cidr_range = "172.16.0.0/24"
+  ip_cidr_range = "10.1.0.0/24"
   network       = google_compute_network.back_office.self_link
   region        = var.region
+}
+
+# firewall rule for back office: iap
+resource "google_compute_firewall" "back_office_iap" {
+  name    = "back-office-iap"
+  network = google_compute_network.back_office.self_link
+
+  source_ranges = ["35.235.240.0/20"]
+  direction     = "INGRESS"
+  allow {
+    protocol = "tcp"
+  }
+}
+# firelwall rule for back office: ssh
+resource "google_compute_firewall" "back_office_ssh" {
+  name    = "back-office-ssh"
+  network = google_compute_network.back_office.self_link
+
+  source_ranges = ["0.0.0.0/0"]
+  direction     = "INGRESS"
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+}
+
+# firewall rule for back office: icmp
+resource "google_compute_firewall" "back_office_icmp" {
+  name    = "back-office-icmp"
+  network = google_compute_network.back_office.self_link
+
+  source_ranges = ["0.0.0.0/0"]
+  direction     = "INGRESS"
+  allow {
+    protocol = "icmp"
+  }
 }
 
 # vpc: services
@@ -33,7 +69,43 @@ resource "google_compute_network" "services" {
 # subnet for services
 resource "google_compute_subnetwork" "services" {
   name          = "services"
-  ip_cidr_range = "172.16.0.0/24"
+  ip_cidr_range = "10.1.0.0/24"
   network       = google_compute_network.services.self_link
   region        = var.region
+}
+
+# firewall rule for services
+resource "google_compute_firewall" "services_ssh" {
+  name    = "services-ssh"
+  network = google_compute_network.services.self_link
+
+  source_ranges = ["0.0.0.0/0"]
+  direction     = "INGRESS"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+}
+
+resource "google_compute_firewall" "services_icmp" {
+  name    = "services-icmp"
+  network = google_compute_network.services.self_link
+
+  source_ranges = ["0.0.0.0/0"]
+  direction     = "INGRESS"
+  allow {
+    protocol = "icmp"
+  }
+}
+
+resource "google_compute_firewall" "services_iap" {
+  name    = "services-iap"
+  network = google_compute_network.services.self_link
+
+  source_ranges = ["35.235.240.0/20"]
+  direction     = "INGRESS"
+  allow {
+    protocol = "tcp"
+  }
 }
--- gcp/vms/main.tf
+++ gcp/vms/main.tf
@@ -1,6 +1,6 @@
 # VM in back office network
-resource "google_compute_instance" "bo_vm_test" {
-  name         = "bo-vm-test"
+resource "google_compute_instance" "bo_vm_test_alpha" {
+  name         = "bo-vm-test-alpha"
   machine_type = "f1-micro"
   zone         = var.zone
   tags         = ["bo-vm-test"]
@@ -19,7 +19,29 @@ resource "google_compute_instance" "bo_vm_test" {
   network_interface {
     network    = data.terraform_remote_state.network.outputs.vpc_back_office.id
     subnetwork = data.terraform_remote_state.network.outputs.vpc_back_office_subnetwork.id
-    access_config {}
+  }
+}
+
+resource "google_compute_instance" "bo_vm_test_beta" {
+  name         = "bo-vm-test-beta"
+  machine_type = "f1-micro"
+  zone         = var.zone
+  tags         = ["bo-vm-test"]
+
+  scheduling {
+    preemptible        = true
+    automatic_restart  = false
+    provisioning_model = "SPOT"
+  }
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+  network_interface {
+    network    = data.terraform_remote_state.network.outputs.vpc_back_office.id
+    subnetwork = data.terraform_remote_state.network.outputs.vpc_back_office_subnetwork.id
   }
 }
 
@@ -44,7 +66,6 @@ resource "google_compute_instance" "services_vm_test" {
   network_interface {
     network    = data.terraform_remote_state.network.outputs.vpc_services.id
     subnetwork = data.terraform_remote_state.network.outputs.vpc_services_subnetwork.id
-    access_config {}
   }
 }
 
