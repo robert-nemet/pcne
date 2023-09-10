949070b021cf92c672196dbdaac4e271e0cb6800
Robert Nemet <robert.nemet@gmail.com>
2023-09-10 12:58:29 +0200

    migrate base for dev

--- dev/base/outputs.tf
+++ dev/base/outputs.tf
@@ -1,7 +1,3 @@
-output "gcs_account_email" {
-  value = data.google_storage_project_service_account.gcs_account.email_address
-}
-
 output "region" {
   value = var.region
 }
--- dev/base/provider.tf
+++ dev/base/provider.tf
@@ -17,6 +17,6 @@ terraform {
 
   backend "gcs" {
     bucket = "terraform-states-network-playground-382512"
-    prefix = "terraform/state/base"
+    prefix = "terraform/state/dev/base"
   }
 }
