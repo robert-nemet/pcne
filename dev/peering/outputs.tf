89ddc6faf36ae0c9445f9f60e3a436f5ee0b0f71
Robert Nemet <robert.nemet@gmail.com>
2023-09-10 14:08:28 +0200

    migrate network services and storage

--- dev/peering/outputs.tf
+++ dev/peering/outputs.tf
@@ -13,3 +13,20 @@ output "vpc_back_office_private_subnetwork" {
 output "back_office_fw_sa" {
   value = google_service_account.back_office_fw_sa.email
 }
+
+output "vpc_services" {
+  value = google_compute_network.services
+}
+
+output "vpc_services_subnetwork" {
+  value = google_compute_subnetwork.services
+}
+
+output "vpc_storage" {
+  value = google_compute_network.storage
+}
+
+output "vpc_storage_subnetwork" {
+  value = google_compute_subnetwork.subnet_postgres
+}
+
--- dev/storage/network/outputs.tf
+++ dev/storage/network/outputs.tf
@@ -1,27 +1,3 @@
-output "vpc_back_office" {
-  value = google_compute_network.back_office
-}
-
-output "vpc_back_office_subnetwork" {
-  value = google_compute_subnetwork.back_office
-}
-
-output "vpc_back_office_private_subnetwork" {
-  value = google_compute_subnetwork.back_office_private
-}
-
-output "back_office_fw_sa" {
-  value = google_service_account.back_office_fw_sa.email
-}
-
-output "vpc_services" {
-  value = google_compute_network.services
-}
-
-output "vpc_services_subnetwork" {
-  value = google_compute_subnetwork.services
-}
-
 output "vpc_storage" {
   value = google_compute_network.storage
 }
